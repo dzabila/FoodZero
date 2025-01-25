cae5465f25f766b590389eb8e170fa63af4da029
dzabila <giorgi.dzabilashvili.1@btu.edu.ge>
2025-01-25 17:40:05 +0400

    add api links

--- script.js
+++ script.js
@@ -8,3 +8,47 @@ burgerMenu.addEventListener('click',function(){
 function scrollToTop() {
   window.scroll({top: 0, left: 0, behavior: 'smooth'});
 }
+//api 
+const description = document.querySelector(".section__four--project--paragraph");
+const description2 = document.querySelector(".section__four--project--paragraph2")
+const description3 = document.querySelector(".section__four--project--paragraph3")
+const description4 = document.querySelector(".section__four--project--paragraph4")
+const image = document.querySelector(".section__four--project");
+const image2 = document.querySelector(".section__four--project2");
+const image3 = document.querySelector(".section__four--project3");
+const image4 = document.querySelector(".section__four--project4");
+let requist = new XMLHttpRequest();
+requist.open("GET", "https://reqres.in/api/users?page=2");
+requist.addEventListener("load", function() {
+  let response = this.response;
+  let responseData = JSON.parse(response);
+  description.textContent = `Author: ${responseData.data[0].first_name} ${responseData.data[0].last_name}`;
+  description2.textContent = `Author: ${responseData.data[1].first_name} ${responseData.data[1].last_name}`;
+  description2.style.color = "white";
+  description2.style.fontSize = "20px"
+  description3.textContent = `Author: ${responseData.data[2].first_name} ${responseData.data[2].last_name}`;
+   description3.style.color = "white";
+  description3.style.fontSize = "20px"
+  description4.textContent = `Author: ${responseData.data[3].first_name} ${responseData.data[3].last_name}`;
+   description4.style.color = "white";
+  description4.style.fontSize = "20px"
+
+})
+requist.addEventListener("error", function() {
+  console.log("error")
+});
+requist.send();
+let requist1 = new XMLHttpRequest();
+requist1.open("GET", "https://reqres.in/api/unknown");
+requist1.addEventListener("load", function(){
+  let response = this.response;
+  let responseData = JSON.parse(response);
+  image.style.backgroundColor = responseData.data[0].color;
+  image2.style.backgroundColor = responseData.data[1].color;
+  image3.style.backgroundColor = responseData.data[2].color;
+  image4.style.backgroundColor = responseData.data[3].color;
+});
+requist1.addEventListener("error", function() {
+  console.log("error")
+});
+requist1.send();
